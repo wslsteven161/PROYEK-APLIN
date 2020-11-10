920f51bbab11b8dc79fe15c16534d7499b2ad920
Jeremy <adgrygry12348@gmail.com>
2020-11-10 19:14:57 +0700

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -22,4 +22,56 @@
     {
         throw new PDOException($e->getMessage(), (int)$e->getCode());
     }
+
+    // EXAMPLES 
+    /*
+
+    // QUERY
+
+        // SELECT
+    $result = $pdo->query('SELECT name FROM users');
+    while ($row = $result->fetch())
+    {
+        echo $row['name'] . "\n";
+    }
+
+       // UPDATE
+    $sql = "UPDATE users SET name = ? WHERE id = ?";
+    $pdo->prepare($sql)->execute([$name, $id]); 
+
+        // DELETE
+    $stmt = $pdo->prepare("DELETE FROM goods WHERE category = ?");
+    $stmt->execute([$cat]);
+    $deleted = $stmt->rowCount();
+
+    // PREPARE
+    $stmt = $pdo->prepare('SELECT * FROM users WHERE email = ? AND status=?');
+    $stmt->execute([$email, $status]);
+    $user = $stmt->fetch();
+    // or
+    $stmt = $pdo->prepare('SELECT * FROM users WHERE email = :email AND status=:status');
+    $stmt->execute(['email' => $email, 'status' => $status]);
+    $user = $stmt->fetch();
+
+    // GETTING DATA
+    $stmt = $pdo->query('SELECT name FROM users');
+    foreach ($stmt as $row)
+    {
+        echo $row['name'] . "\n";
+    }
+
+        // FETCH ASSOC
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // FETCH CLASS
+        $news = $pdo->query('SELECT * FROM news')->fetchAll(PDO::FETCH_CLASS, 'News');
+
+        // getting number of rows in the table utilizing method chaining
+        $count = $pdo->query("SELECT count(*) FROM table")->fetchColumn();
+
+        // FETCH ALL
+        $data = $pdo->query('SELECT id, name FROM users')->fetchAll(PDO::FETCH_KEY_PAIR);
+
+        
+    */
 ?>
