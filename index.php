f5ad5f8e6fa25db3ea1dd8916597116b723a884b
Jeremy <adgrygry12348@gmail.com>
2020-11-10 18:01:35 +0700

    edit

--- index.php
+++ index.php
@@ -10,6 +10,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>APOTEK</title>
 
+    <!-- JQUERY -->
+    <script src="js/jquery-3.5.1.min.js"></script>
+
     <!-- BOOTSTRAP -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="js/bootstrap.bundle.min.js"></script>
@@ -18,11 +21,11 @@
     <link rel="stylesheet" href="css/all.min.css">
     <script src="js/all.min.js"></script>
 
-    <!-- JQUERY -->
-    <script src="js/jquery-3.5.1.min.js"></script>
-
     <!-- STYLE -->
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- LITTLE <3 ICON -->
+    <link href="images/favicon.ico" rel="icon" type="image/x-icon" />
 
 </head>
 <body>
