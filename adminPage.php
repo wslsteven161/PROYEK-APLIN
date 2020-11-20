838cace01e1ee6966325bc6c64f803fae63138ce
Jeremy <adgrygry12348@gmail.com>
2020-11-20 20:22:07 +0700

    Update adminPage.php

--- adminPage.php
+++ adminPage.php
@@ -35,8 +35,39 @@
 
     <!-- SWEET ALERT -->
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+    <style>
+        .adm-front{
+            margin: 100px auto;
+            text-align: center;
+            justify-content: center;
+        }
+    </style>
 </head>
 <body>
-    
+    <header class="fixed-top d-flex align-items-center header-transparent">
+        <div class="container d-flex align-items-center">
+            <div class="logo mr-auto">
+                <h1>APOTEK</h1>
+                <!--a href="#"><img src="" alt="" class="img-fluid"></a-->
+            </div>
+
+            <nav class="nav-menu d-none d-lg-block">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Profile</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <div class="FRONT-PAGE adm-front">
+        <h1>Welcome, Admin!</h1>
+    </div>
+
+    <div class="container">
+        <h1>TEST</h1>
+    </div>
+
 </body>
 </html>
