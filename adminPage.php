89937957e83f12797d95a141cf0752388f77b1aa
Jeremy <adgrygry12348@gmail.com>
2020-11-21 15:02:56 +0700

    Update adminPage.php

--- adminPage.php
+++ adminPage.php
@@ -14,7 +14,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin Page</title>
 
     <!-- JQUERY -->
     <script src="js/jquery-3.5.1.min.js"></script>
@@ -41,6 +41,11 @@
             margin: 100px auto;
             text-align: center;
             justify-content: center;
+            color: #EF8354;
+        }
+        body {
+            background: #2D3142;
+            font-family: 'Raleway', sans-serif;
         }
     </style>
 </head>
@@ -66,7 +71,38 @@
     </div>
 
     <div class="container">
-        <h1>TEST</h1>
+        
+        <a id="button-1" class="button" href="#">Let's Go!</a>
+        <div class="button" id="button-2">
+            <div id="slide"></div>
+            <a href="#">Let's Go!</a>
+        </div>
+
+        <div class="button" id="button-3">
+            <div id="circle"></div>
+            <a href="#">Let's Go!</a>
+        </div>
+
+        <div class="button" id="button-4">
+            <div id="underline"></div>
+            <a href="#">Let's Go!</a>
+        </div>
+
+        <div class="button" id="button-5">
+            <div id="translate"></div>
+            <a href="#">Let's Go!</a>
+        </div>
+
+        <div class="button" id="button-6">
+            <div id="spin"></div>
+            <a href="#">Let's Go!</a>
+        </div>
+
+        <div class="button" id="button-7">
+            <div id="dub-arrow"><img src="#" alt="" /></div>
+            <a href="#">Let's Go!</a>
+        </div>
+
     </div>
 
 </body>
